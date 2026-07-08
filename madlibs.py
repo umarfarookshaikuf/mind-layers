925da668650910fab9df1ec486111003db37b2c6
umarfarookshaikuf <509ufaook@gmail.com>
2026-07-08 16:32:26 +0530

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+
+print("hi,welcome to the madlibs game !")
+
+user_name = input("what is your name ?")
+fav_color = input("what is your favorite color ?")
+fav_number = input("what is your favorite number ?")
+fav_number = int(fav_number)
+_hobby = input("what is your hobby ?")
+
+
+print("HI,I'm ",user_name,"!"
+"My favorite color is", fav_color,
+",my lucky number is", fav_number,
+"and i like to",_hobby)
+
+
+
+
+
+
+
